2eade5b2c987993ac5c0391cb075f23bbee5ff2d
flakster <carlos.santamaria@gmail.com>
2020-04-07 11:11:20 -0500

    fix database issue

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_04_06_211502) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
